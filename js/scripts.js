176f88cb76b1630b4c2ead1dfea0962ffd5f005b
Yit Chee Chin <yitchee1998@gmail.com>
2019-08-13 04:05:30 +0100

    Added contact form, skills, footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
 	$('.sidenav').sidenav();
+	$('.collapsible').collapsible();
+
+	var elem = document.querySelector('.collapsible.expandable');
+	var instance = M.Collapsible.init(elem, {
+  		accordion: false
+	});
 });
 
-$(document).ready(function(){
-	$('.collapsible').collapsible();
+/* function for changing Project header colours on click */
+var prevProjectClickTarget = null;
+$('#projects .collapsible-header').click(function(){
+	if (this == prevProjectClickTarget && $(this).hasClass('orange lighten-1')) {
+		$(this).removeClass('orange lighten-1');
+		prevProjectClickTarget = null
+	}
+	else {
+		$('.collapsible-header').removeClass('orange lighten-1');
+		$(this).addClass('orange lighten-1');
+		prevProjectClickTarget = this;
+	}
 });
