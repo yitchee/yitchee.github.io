8961baf046f40304d65e4c8ef14085ab9df55474
Yit Chee Chin <yitchee1998@gmail.com>
2019-08-15 20:55:27 +0100

    Added screenshots to Projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 	$('.sidenav').sidenav();
 	$('.collapsible').collapsible();
-	$('.tabs').tabs();
+	$('.tabs').tabs({
+  		onShow: function(tab) {
+    		$('.carousel').carousel({
+    			fullWidth: true,
+    			noWrap: true,
+    			indicators: true
+  			});
+  		}
+	});
 
 	var elem = document.querySelector('.collapsible.expandable');
 	var instance = M.Collapsible.init(elem, {
@@ -12,22 +20,37 @@ $(document).ready(function(){
 /* function for changing Project header colours on click */
 var prevProjectClickTarget = null;
 $('#projects .collapsible-header').click(function(){
-	if (this == prevProjectClickTarget && $(this).hasClass('orange lighten-1')) {
-		$(this).removeClass('orange lighten-1');
+
+	if (this == prevProjectClickTarget && $(this).hasClass('orange lighten-2')) {
+		$(this).removeClass('orange lighten-2');
 		prevProjectClickTarget = null
 	}
 	else {
-		$('.collapsible-header').removeClass('orange lighten-1');
-		$(this).addClass('orange lighten-1');
+		$('.collapsible-header').removeClass('orange lighten-2');
+		$(this).addClass('orange lighten-2');
 		prevProjectClickTarget = this;
 	}
 });
 
-$('#skills .collapsible-header').click(function(){
+$('#skills .collapsible-header').click( function(){
 	if ($(this).hasClass('light-blue lighten-2')) {
 		$(this).removeClass('light-blue lighten-2');
 	}
 	else {
 		$(this).addClass('light-blue lighten-2');
 	}
+});
+
+// move next carousel
+$('.moveNextCarousel').click( function(e){
+	e.preventDefault();
+	e.stopPropagation();
+	$('.carousel').carousel('next');
+});
+
+// move prev carousel
+$('.movePrevCarousel').click( function(e){
+	e.preventDefault();
+	e.stopPropagation();
+	$('.carousel').carousel('prev');
 });
